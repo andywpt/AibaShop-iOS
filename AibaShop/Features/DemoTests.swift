2a42a5cd87f2eb522e3161464cd84927073af8d2
Andy <andywu.england@gmail.com>
2024-10-06 18:50:45 +0800

    Add unit tests

---
+++ AibaShop/Features/DemoTests.swift
@@ -0,0 +1,21 @@
+@testable import AibaShop_iOS
+import XCTest
+
+final class DemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test1() throws {
+        XCTAssertEqual(1+1, 2)
+    }
+    
+    func test2() throws {
+        XCTAssertEqual(1+1, 2)
+    }
+}
