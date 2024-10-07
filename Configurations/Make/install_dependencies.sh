48538e656ccfdc4cb113b55c5540a504352d35e0
Andy <andywu.england@gmail.com>
2024-10-07 21:27:34 +0800

    add mint

--- Configurations/Make/install_dependencies.sh
+++ Configurations/Make/install_dependencies.sh
@@ -5,8 +5,9 @@ if ! command -v brew &> /dev/null; then
     exit 1
 fi
 export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
 brew update --quiet
-packages=(gnupg cocoapods fastlane swiftformat swiftgen sourcery xcodegen)
+packages=(gnupg mint cocoapods fastlane swiftformat swiftgen sourcery xcodegen)
 for package in "${packages[@]}"; do
     brew install --formula "$package" || true
 done
