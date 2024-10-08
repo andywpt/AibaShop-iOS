8f305ac677e4cb1e4173c76305a58ea02797e86c
Andy Wu <130017974+andywpt@users.noreply.github.com>
2024-10-08 15:43:37 +0800

    chore: Only checks if cache entry exists and skips download (#28)

--- Configurations/Make/install_dependencies.sh
+++ Configurations/Make/install_dependencies.sh
@@ -4,8 +4,6 @@ if ! command -v brew &> /dev/null; then
     exit 1
 fi
 export HOMEBREW_NO_AUTO_UPDATE=1
-export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
-# brew update --quiet
 packages=(gnupg swiftformat swiftgen sourcery xcodegen)
 for package in "${packages[@]}"; do
     brew install "$package" || true
